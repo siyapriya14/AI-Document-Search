d2dbc5abf81c6a3f0a9b1cc01cc0c67a5715c832
siyapriya14 <siya87774@gmail.com>
2026-01-29 18:40:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 import streamlit as st
 from rag_pipeline import get_answer_from_docs
+import os
 
 st.title("AI Document Search using RAG (Demo)")
 
+# Upload file
+uploaded_file = st.file_uploader(
+    "Upload a PDF document",
+    type=["pdf"]
+)
+
 query = st.text_input("Ask a question from the document:")
 
-if query:
-    answer = get_answer_from_docs(query)
+if uploaded_file and query:
+    # Save uploaded file
+    os.makedirs("data", exist_ok=True)
+    file_path = os.path.join("data", uploaded_file.name)
+
+    with open(file_path, "wb") as f:
+        f.write(uploaded_file.read())
+
+    answer = get_answer_from_docs(query, file_path)
     st.success(answer)
